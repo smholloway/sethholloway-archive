4e66a731740339832a53948ec37844540dd486aa
Seth Holloway <smholloway@gmail.com>
2011-02-15 18:55:41 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sethholloway::Application.config.secret_token = '52aa8088cd268812fca458aecbbe0b88f0e537c6807bf1f8644fec6b59885a2428af3ea38f534e36aad9e5ee8ade4c2888d931876168c7938d056d0b8af5b1ad'
